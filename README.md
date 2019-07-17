75b81a508799828e2fc9ef5667f4519b9cb77b69
p3rob <robert.cini@gig.com>
2019-07-17 10:57:49 +0200

    --Changed README.md for current task --Changed index.js for more readability and operability --Finalised task

--- README.md
+++ README.md
@@ -1,19 +1,16 @@
-# Displaying data from API with Node JS
+# Creating a CLI with Inquirer and Node JS
 
 ## Description
-For the task, **Axios** needs to be installed in the directory of the source files. This is so the HTTP calls can be made to the API. When the project is started with `npm start`, data will be displayed from the following API :- **swapi**.
-
-**Postman** can also be utilised to simulate and make calls to the API that is currently being used. This helps to validate multiple things that you may be looking for, in order for your main program to successfully utilise the API.
+### Setup
+For the task, **Axios** and the **Inquirer** needs to be installed in the directory of the source files. Axios is utilised so that HTTP calls can be made to the API. Inquirer is utilised for the displaying of data in a list and the ability of the user to select from the list. The project was required to be executed with `starwars` rather than `npm start`. This was done by adding a `bin:{"starwars": pathOfIndex.js}` to the **package.json** file. Also a link is required to be made with the use of `npm link` to make all of this possible.
+### Task
+User will be greeted by a list featuring all of the spaceships that exist in the Starwars API :- **swapi**. After one is chosen, the pilots of the ship will be displayed. If none are found, a message will appear displaying as such. If the pilot exists and one is chosen, the details of the pilot will then be displayed to the user.
 
 
 ## Table
 
-```markdown
-
-|   Task Name  | Task Desc                                      | Output     |   |   |
-|------------: |------------------------------------------------|------------|---|---|
-| `data fetch` | List of Starships and Pilots will be displayed | Starship 1 |   |   |
-|              |                                                | Pilot 1    |   |   |
-|              |                                                | Pilot 2    |   |   |
-```
-
+| Task Name        | Task Desc                  | Output                                                |   |   |
+|------------------|----------------------------|-------------------------------------------------------|---|---|
+| `starwars`       | Program will execute       | List of Spaceships will be displayed                  |   |   |
+| Spaceship chosen | User will pick a spaceship | List of Pilots of the chosen spaceship (if available) |   |   |
+| Pilot chosen     | User will pick a pilot     | Properties of the chosen pilot will be displayed      |   |   |
--- bin/index.js
+++ bin/index.js
@@ -1,81 +1,52 @@
 #!/usr/bin/env node
+
 const axios = require('axios');
 const inquirer = require('inquirer');
 
+getAPI();
 
-let starShipResponse = [];
-let starShipsAPI;
+async function getAPI() {
 
+ let StarshipUrl = `https://swapi.co/api/starships/`;
+ const starShipResponse = [];
 
-; (async () => {
- for (i = 1; i <= 4; i++) {
+ while (StarshipUrl != null) {
 
+   const starShipsAPI = await axios.get(StarshipUrl);
 
-   starShipsAPI = await axios({
+   for (i = 0; i < starShipsAPI.data.results.length; i++) {
+     let ShipName = starShipsAPI.data.results[i].name;
+     let PilotUrls = starShipsAPI.data.results[i].pilots;
 
-     url: `https://swapi.co/api/starships/?page=${i}`,
-     method: 'get'
-
-   });
-
-   starShipResponse.push(...starShipsAPI.data.results);
-
- }
-
- // function arrayToObject(array, keyField) {
- //   let finalObj = {};
-
-
-
- //   array.forEach(element => {
- //     const newKey = element[keyField];
-
- //     finalObj = {
- //       ...finalObj,
- //       [newKey]: element
- //     };
- //   });
-
- //   return finalObj;
-
- // };
-
- //const ShipObj = arrayToObject(starShipResponse, "name");
-
-
- function getStarships() {
-   let shipObj = [];
-   for (i = 0; i < starShipResponse.length; i++) {
-
-
-     shipObj.push({
-       name: starShipResponse[i].name,
+     starShipResponse.push({
+       name: ShipName,
        value: {
-         name: starShipResponse[i].name,
-         url: starShipResponse[i].pilots
+         name: ShipName,
+         url: PilotUrls
        }
      });
 
-     // shipObj = {...shipObj,
-     //   [newKey]: [
-     //     name = starShipResponse[i].name,
-     //     value = {
-     //     url = gettingPilots
-     //     }
-     //   ]
-     // }
    }
+   StarshipUrl = starShipsAPI.data.next;
 
-   //  console.log(shipObj);
-   return shipObj;
+ }
+ InquirerQ(starShipResponse);
+}
+
+function InquirerQ(starShipResponse) {
+  inquirer.prompt(Questions(starShipResponse)).then(answers => {
+    console.info('Ship Selected:', answers.Starships.name);
+    console.info('Pilot Selected:', answers.Pilots.name);
+    console.info('Pilot Details', answers.Pilots);
+  });
  }
 
-
- let StarshipQ = [{
+function Questions(starShipResponse) {
+ return [{
    type: 'list',
    name: 'Starships',
    message: 'Select a Starship',
-   choices: getStarships()
+   choices: starShipResponse
  },
  {
    type: 'list',
@@ -84,44 +55,38 @@ let starShipsAPI;
    choices(answers) { return GetPilots(answers.Starships) }
  }];
 
- async function GetPilots(StarshipKey) {
-   StarshipKey = StarshipKey.url;
+}
 
-   let array = [];
+async function GetPilots(StarshipKey) {
+ StarshipKey = StarshipKey.url;
 
-   if(StarshipKey.length === 0) {
-     array.push('No Pilots pilot this Ship');
-     return array;
-   }
-   // else {
-   for (i = 0; i < StarshipKey.length; i++) {
-     let pilot = await axios({
-       url: StarshipKey[i],
-       method: 'get'
-     });
-     array.push({
+ let array = [];
+
+ if (StarshipKey.length === 0) {
+   array.push({
+     name: "No Pilots",
+     value: {
+       name: "No Details found",
+
+     }
+   });
+   return array;
+ }
+
+ for (i = 0; i < StarshipKey.length; i++) {
+   let pilot = await axios({
+     url: StarshipKey[i],
+     method: 'get'
+   });
+   array.push({
+     name: pilot.data.name,
+     value: {
        name: pilot.data.name,
-       value: {
-         name: pilot.data.name,
-         height: pilot.data.height
-       }
-     });
-   }
- //}
-   //console.log(array);
-   return array
+       height: pilot.data.height
+     }
+   });
+ }
+ return array
 
- };
+};
 
-
-
- inquirer.prompt(StarshipQ).then(answers => {
-   console.info('Ship Selected:', answers.Starships.name);
-   console.info('Pilot Selected:', answers.Pilots.name);
-   console.info('Pilot Details', answers.Pilots);
-
-
- });
-
-
-})()
